34c006c37785f94549298475df60918ec5f6ff26
lvye <lvye@idwzx.com>
2017-12-11 11:12:05 +0800

    dubbo 测试
    dubbo 测试数据

---
+++ dubbo-api/src/main/java/com/alibaba/dubbo/demo/Apple.java
@@ -0,0 +1,63 @@
+package com.alibaba.dubbo.demo;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by BISHUILANTIAN on 2017/12/1.
+ */
+public class Apple {
+    private Integer id;
+    private String name;
+    private BigDecimal money;
+    private Integer num;
+    public Apple(Integer id, String name, BigDecimal money, Integer num) {
+        this.id = id;
+        this.name = name;
+        this.money = money;
+        this.num = num;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public void setMoney(BigDecimal money) {
+        this.money = money;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Apple{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", money=" + money +
+                ", num=" + num +
+                '}';
+    }
+}
